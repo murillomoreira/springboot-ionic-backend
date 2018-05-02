3bd02e6eb7158bab3fbed990d16a5a17a8813f9d
Murillo Moreira <ppmurillomoreira@gmail.com>
2018-05-02 17:15:44 -0300

    Endpoin /clientes/{id} disponível

---
+++ src/main/java/com/mamc/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.mamc.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.mamc.cursomc.domain.Cliente;
+import com.mamc.cursomc.repositories.ClienteRepository;
+import com.mamc.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;	
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto Não encontrata Id: " + id + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
+}
