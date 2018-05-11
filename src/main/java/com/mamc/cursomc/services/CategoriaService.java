5fb206164cfe0ed2b02cf884579c96a9fbfd5145
Murillo Moreira <ppmurillomoreira@gmail.com>
2018-05-11 11:29:43 -0300

    Validação sintática de dados com Bean Validation

--- src/main/java/com/mamc/cursomc/services/CategoriaService.java
+++ src/main/java/com/mamc/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mamc.cursomc.domain.Categoria;
+import com.mamc.cursomc.dto.CategoriaDTO;
 import com.mamc.cursomc.repositories.CategoriaRepository;
 import com.mamc.cursomc.services.exceptions.DataIntegrityException;
 import com.mamc.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,4 +58,9 @@ public class CategoriaService {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
+	}
+	
 }
