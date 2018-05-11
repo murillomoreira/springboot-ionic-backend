4e0c59e2b7fd07d8a624db4f741e4e225537acfa
Murillo Moreira <ppmurillomoreira@gmail.com>
2018-05-11 17:11:03 -0300

    Validação customizada na inserção de Cliente(email repetido)

--- src/main/java/com/mamc/cursomc/domain/Cliente.java
+++ src/main/java/com/mamc/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,9 +25,10 @@ public class Cliente implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
+	private Integer id;	
 	private String nome;
+	
+	@Column(unique=true)
 	private String email;
 	private String cpfOuCnpj;	
 	private Integer tipo;
