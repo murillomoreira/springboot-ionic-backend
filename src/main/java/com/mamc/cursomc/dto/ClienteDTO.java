9e983c2a2b35b14e8e5cd3a558ac6663280c577e
Murillo Moreira <ppmurillomoreira@gmail.com>
2018-05-11 17:30:03 -0300

    Validação customizada na atualização de Cliente(email repetido)

--- src/main/java/com/mamc/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/mamc/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.mamc.cursomc.domain.Cliente;
+import com.mamc.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
